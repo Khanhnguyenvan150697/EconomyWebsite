d03fceee2c22667f170f1b18c7ca56ad255b4689
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2019-10-12 22:48:47 +0700

    register_login

---
+++ Economy/Economy/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Economy.Common;
+using Economy.Models;
+
+namespace Economy.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Session = Session[CommonConstant.USER_SESSION];
+            return View();
+        }
+    }
+}
---
+++ Economy/Model/EF/User.cs
@@ -0,0 +1,50 @@
+namespace Model.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        public long ID { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage ="Enter your username, please!")]
+        public string UserName { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "Enter your password, please!")]
+        public string Password { get; set; }
+
+        [StringLength(100)]
+        public string Salt { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(250)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Enter your email, please!")]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        [StringLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        [StringLength(50)]
+        public string ModifiedBy { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
