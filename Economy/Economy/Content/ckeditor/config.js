63fe7376c4e6e4341e332df9a7292a5ffac109ed
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2019-10-19 22:36:58 +0700

    CKEditor_CKFinder

--- Economy/Economy/Content/ckeditor/config.js
+++ Economy/Economy/Content/ckeditor/config.js
@@ -1,12 +1,12 @@
 /**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    // config.uiColor = '#AADC6E';
 
     config.syntaxhighlight_lang = 'csharp';
     config.syntaxhighlight_hideControls = true;
@@ -15,7 +15,7 @@ CKEDITOR.editorConfig = function( config ) {
     config.filebrowserImageBrowseUrl = '/Content/ckfinder/ckfinder.html?Types=Images';
     config.filebrowserFlashBrowseUrl = '/Content/ckfinder/ckfinder.html?Types=Flash';
     config.filebrowserUploadUrl = '/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=File';
-    config.filebrowserImageUploadUrl = '/Content/Photos';
+    config.filebrowserImageUploadUrl = '/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Image';
     config.filebrowserFlashUploadUrl = '/Content/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
 
     CKFinder.setupCKEditor(null, '/Content/ckfinder/');
---
+++ Economy/Economy/Scripts/Cktool.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    CKEDITOR.replace('txtNoidung');
+    $('#selectImg').click(function () {
+        var finder = new CKFinder();
+        finder.selectActionFunction = function (fileUrl) {
+            //code js here
+            $('#linkUrlImg').val(fileUrl);
+        };
+        finder.popup();
+    });
+});
