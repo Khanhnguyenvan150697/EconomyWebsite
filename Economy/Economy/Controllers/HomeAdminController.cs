a1ffe07ffa3730e038159fbcce4a006c7905cd2b
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2019-10-21 14:01:52 +0700

    PagedList

--- Economy/Economy/Controllers/HomeAdminController.cs
+++ Economy/Economy/Controllers/HomeAdminController.cs
@@ -5,18 +5,23 @@ using System.Web;
 using System.Web.Mvc;
 using Model.EF;
 using Economy.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace Economy.Controllers
 {
     public class HomeAdminController : Controller
     {
         // GET: HomeAdmin
-        public ActionResult HomeAdmin()
+        public ActionResult HomeAdmin(int? page)
         {
             EconomyDbContext db = null;
             db = new EconomyDbContext();
 
-            var item = db.Products.ToList();
+            const int PAGESIZE = 5;
+            int pageNumber = (page ?? 1);
+
+            var item = db.Products.OrderBy(x => x.ID).ToPagedList(pageNumber, PAGESIZE);
 
             return View(item);
         }
