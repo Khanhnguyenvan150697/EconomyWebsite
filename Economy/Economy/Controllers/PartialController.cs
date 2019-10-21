36e290f57d446742f674c34a3da3ea71279bbfc3
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2019-10-21 10:32:53 +0700

    UpdateAndDeleteProduct

--- Economy/Economy/Controllers/PartialController.cs
+++ Economy/Economy/Controllers/PartialController.cs
@@ -13,6 +13,7 @@ namespace Economy.Controllers
 {
     public class PartialController : Controller
     {
+        EconomyDbContext db = new EconomyDbContext();
         // GET: Partial
         public ActionResult Account()
         {
@@ -24,73 +25,7 @@ namespace Economy.Controllers
             ViewBag.Session = Session[Common.Admin.CommonConstant.ADMIN_SESSION];
             return PartialView();
         }
-
-
-        [HttpGet]
-        public ActionResult PopupModal()
-        {
-            EconomyDbContext db = null;
-            db = new EconomyDbContext();
-
-            ViewBag.Brand = new SelectList(GetBrand(), "Name", "Name");
-            ViewBag.Cates = new SelectList(GetCate(), "Name", "Name");
-            return PartialView();
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult PopupModal(AddNewProduct prod)
-        {
-            EconomyDbContext db = null;
-            db = new EconomyDbContext();
-
-            // new SelectList(ListData,"field saved to CSDL","field displayed")
-
-            ViewBag.Brand = new SelectList(GetBrand(), "Name", "Name");
-            ViewBag.Cates = new SelectList(GetCate(), "Name", "Name");
-
-            if (ModelState.IsValid)
-            {
-                var item = new Product()
-                {
-                    Name = prod.ProductName,
-                    BrandName = prod.BrandName,
-                    CategoryName = prod.CategoryName,
-                    Price = prod.Price,
-                    PromotionPrice = prod.PromotionPrice,
-                    Image = prod.Image,
-                    Description = prod.Description
-                };
-
-                db.Products.Add(item);
-                db.SaveChanges();
-                return RedirectToAction("HomeAdmin","HomeAdmin");
-            }
-            else
-            {
-                return PartialView();
-            }
-        }
-
-        //Get List brands
-        public List<Brand> GetBrand()
-        {
-            EconomyDbContext db = null;
-            db = new EconomyDbContext();
-
-            List<Brand> brands = db.Brands.ToList();
-
-            return brands;
-        }
-
-        //Get List categories
-        public List<Category> GetCate()
-        {
-            EconomyDbContext db = null;
-            db = new EconomyDbContext();
-
-            List<Category> cates = db.Categories.ToList();
-            return cates;
-        }
+    
+       
     }
 }
---
+++ Economy/Economy/Scripts/CKtool_edit.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    CKEDITOR.replace('txtNoidung_edit');
+    $('#selectImg_edit').click(function () {
+        var finder = new CKFinder();
+        finder.selectActionFunction = function (fileUrl) {
+            //code js here
+            $('#linkUrlImg_edit').val(fileUrl);
+        };
+        finder.popup();
+    });
+});
