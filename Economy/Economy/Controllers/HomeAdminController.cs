234ad91e3a4fb60ac287cc4576a55f4d5e675bf0
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2019-10-26 19:17:14 +0700

    deleteProduct_searchDynamically

--- Economy/Economy/Controllers/HomeAdminController.cs
+++ Economy/Economy/Controllers/HomeAdminController.cs
@@ -12,18 +12,120 @@ namespace Economy.Controllers
 {
     public class HomeAdminController : Controller
     {
+        EconomyDbContext db = new EconomyDbContext();
         // GET: HomeAdmin
         public ActionResult HomeAdmin(int? page)
         {
-            EconomyDbContext db = null;
-            db = new EconomyDbContext();
-
             const int PAGESIZE = 5;
             int pageNumber = (page ?? 1);
 
             var item = db.Products.OrderBy(x => x.ID).ToPagedList(pageNumber, PAGESIZE);
 
             return View(item);
+        }
+
+        [HttpGet]
+        public JsonResult ShowData(int page, int pageSize)
+        {
+            var _prod = db.Products.ToList();
+            return Json(_prod, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Brand = new SelectList(GetBrand(), "Name", "Name");
+            ViewBag.Cates = new SelectList(GetCate(), "Name", "Name");
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(AddNewProduct prod)
+        {
+            ViewBag.Brand = new SelectList(GetBrand(), "Name", "Name");
+            ViewBag.Cates = new SelectList(GetCate(), "Name", "Name");
+            if (ModelState.IsValid)
+            {
+                var item = new Product()
+                {
+                    Name = prod.ProductName,
+                    BrandName = prod.BrandName,
+                    CategoryName = prod.CategoryName,
+                    Price = prod.Price,
+                    Quantity = prod.Quantity,
+                    PromotionPrice = prod.PromotionPrice,
+                    Image = prod.Image,
+                    Description = prod.Description,
+                    Detail = prod.Detail
+                };
+                db.Products.Add(item);
+                db.SaveChanges();
+            }
+            return RedirectToAction("HomeAdmin");
+        }
+
+        //Get List brands
+        public List<Brand> GetBrand()
+        {
+
+            List<Brand> brands = db.Brands.ToList();
+
+            return brands;
+        }
+
+        //Get List categories
+        public List<Category> GetCate()
+        {
+            List<Category> cates = db.Categories.ToList();
+            return cates;
+        }
+
+        //Edit product
+        [HttpGet]
+        public ActionResult Edit(long? id)
+        {
+            var prod = db.Products.Find(id);
+
+            ViewBag.Brand = new SelectList(GetBrand(), "Name", "Name");
+            ViewBag.Cates = new SelectList(GetCate(), "Name", "Name");
+
+            if (prod != null)
+            {
+                return View(prod);
+            }
+            else
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("HomeAdmin", "HomeAdmin");
+            }
+            ViewBag.Brand = new SelectList(GetBrand(), "Name", "Name");
+            ViewBag.Cates = new SelectList(GetCate(), "Name", "Name");
+
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Delete(long? id)
+        {
+            string result = string.Empty;
+            var prod = db.Products.Where(x => x.ID == id).FirstOrDefault();
+            if(prod != null)
+            {
+                db.Products.Remove(prod);
+                db.SaveChanges();
+                result = "Xóa thành công";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Economy/Economy/Models/AddNewProduct.cs
+++ Economy/Economy/Models/AddNewProduct.cs
@@ -11,19 +11,32 @@ namespace Economy.Models
     public class AddNewProduct
     {
         public long ID { set; get; }
+
         [Required(ErrorMessage ="Enter your product name...")]
         public string ProductName { set; get; }
+
         [Required(ErrorMessage = "Enter brand name...")]
         public string BrandName { set; get; }
+
         [Required(ErrorMessage ="You don't choose a image")]
         public string Image { set; get; }
+
         [Required(ErrorMessage = "Enter category name...")]
         public string CategoryName { set; get; }
+
         [Required(ErrorMessage = "Enter price...")]
         public decimal Price { set; get; }
+
+        [Required(ErrorMessage = "Enter quantity...")]
+        public int Quantity { set; get; }
+
         [Required(ErrorMessage = "Enter promotion price")]
         public decimal PromotionPrice { set; get; }
+
         [Required(ErrorMessage = "Enter description")]
         public string Description { set; get; }
+
+        [Required(ErrorMessage = "Enter detail")]
+        public string Detail { set; get; }
     }
 }
